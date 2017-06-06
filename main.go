fd1ba8c48462bbe141279f64776f072bedaf84b0
Jake Champlin <jake.champlin.27@gmail.com>
2017-06-06 12:12:52 -0400

    Initial transfer of provider code

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
+import (
+	"github.com/hashicorp/terraform/plugin"
+	"github.com/terraform-providers/terraform-provider-mysql/mysql"
+)
+
 func main() {
-	/*plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: opc.Provider})*/
+	plugin.Serve(&plugin.ServeOpts{
+		ProviderFunc: mysql.Provider})
 }
