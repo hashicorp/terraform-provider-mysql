bfd952ec66b7c0f062ff510f995876b8ba6ec6bd
Joe Stump <joe@joestump.net>
2018-03-08 11:07:53 -0800

    Back out shimmed module path.

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/hashicorp/terraform/plugin"
-	"github.com/sfdc-pcg/terraform-provider-mysql/mysql"
+	"github.com/terraform-providers/terraform-provider-mysql/mysql"
 )
 
 func main() {
