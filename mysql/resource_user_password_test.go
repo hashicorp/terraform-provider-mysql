87576b5e40d05f7c70daede5095abf3d0f8a79ca
Joe Stump <joe.stump@salesforce.com>
2018-05-29 14:24:35 -0700

    Add a basic acceptance test.

---
+++ mysql/resource_user_password_test.go
@@ -0,0 +1,36 @@
+package mysql
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+func TestAccUserPassword_basic(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccUserCheckDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccUserPasswordConfig_basic,
+				Check: resource.ComposeTestCheckFunc(
+					testAccUserExists("mysql_user.test"),
+					resource.TestCheckResourceAttr("mysql_user_password.test", "user", "jdoe"),
+					resource.TestCheckResourceAttrSet("mysql_user_password.test", "encrypted_password"),
+				),
+			},
+		},
+	})
+}
+
+const testAccUserPasswordConfig_basic = `
+resource "mysql_user" "test" {
+  user = "jdoe"
+}
+
+resource "mysql_user_password" "test" {
+  user    = "${mysql_user.test.user}"
+  pgp_key = "keybase:joestump"
+}
+`
