48ed39df5f012897a197ae00981f5ff88220ecad
winebarrel <sugawara@winebarrel.jp>
2020-02-19 14:10:43 +0900

    Add "mysql_tables" data source

--- mysql/provider.go
+++ mysql/provider.go
@@ -99,6 +99,10 @@ func Provider() terraform.ResourceProvider {
 			},
 		},
 
+		DataSourcesMap: map[string]*schema.Resource{
+			"mysql_tables": dataSourceTables(),
+		},
+
 		ResourcesMap: map[string]*schema.Resource{
 			"mysql_database":      resourceDatabase(),
 			"mysql_grant":         resourceGrant(),
